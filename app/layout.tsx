795a5dcb4bb09c3332a187e6ca20d88fc78d2fb5
emily020kim <emily020kim@gmail.com>
2023-11-15 18:37:11 -0800

    set up routes and basic page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
-import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Header>
+        <Sidebar>
           {children}
-        </Header>
+        </Sidebar>
       </body>
     </html>
   )
